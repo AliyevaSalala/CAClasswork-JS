6c991614fcf91e9dc4a26ec9e7160f70b5320ea7
AliyevaSalala <aselale58@gmail.com>
2023-10-30 02:56:22 -0700

    js

---
+++ 01.Data-Types-Variables/main.js
@@ -0,0 +1,25 @@
+let firstName = "Shalala";
+let lastName ="Aliyeva";
+let fullName;
+fullName = firstName + " " + lastName;
+console.log(  fullName);
+// console.log("firstName:", firstName, "lastName:", lastName);
+
+// part 2
+let country ="Azerbaycan";
+let job ="teacher";
+let age ="21";
+let personInfo = fullName + "." +  " I am a " + job +"." +" I live in "+ country +" I am "+ age +" years old.";
+console.log(personInfo)
+
+// Task2
+
+
+let lenght ="16"; //number
+let color ="brown"; //string
+let float ="12.5"; //float
+let like ="true"; //boolean 
+ let x={
+    firstName: "Shalala",
+    color: "Doe",
+ }   //object 
