af8e0e1e4ad1ab86474d7e4938b37334cec4123a
AliyevaSalala <aselale58@gmail.com>
2023-11-23 01:54:46 -0800

    js event

--- 23.11.-dom-events/form.js
+++ 23.11.-dom-events/form.js
@@ -1,4 +1,19 @@
 let email = document.getElementById("exampleInputEmail1");
 let password = document.getElementById("exampleInputPassword1");
 let form = document.querySelector(".form");
+let checkBox = document.getElementById("exampleCheck1");
 
+form.addEventListener("submit", function (event) {
+  console.log(checkBox);
+  event.preventDefault();
+
+  if (email.value !== "" && password.value !== "") {
+    let user = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(user);
+  } else {
+    alert("input fields can not be empty");
+  }
+});
