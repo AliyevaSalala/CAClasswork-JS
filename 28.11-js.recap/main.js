904d661996384b89dd4f176eadbdf258cae7f761
AliyevaSalala <aselale58@gmail.com>
2023-11-28 01:25:05 -0800

    js

---
+++ 28.11-js.recap/main.js
@@ -0,0 +1,33 @@
+const employees = [
+  {
+    name: "Jamil",
+    salary: 50000,
+    department: "IT",
+  },
+  {
+    name: "Jale",
+    salary: 60000,
+    department: "HR",
+  },
+  {
+    name: "Bayram",
+    salary: 50000,
+    department: "IT",
+  },
+  {
+    name: "Jamil",
+    salary: 50000,
+    department: "IT",
+  },
+  {
+    name: "Jamil",
+    salary: 50000,
+    department: "IT",
+  },
+];
+
+
+const fruits=["banana", "orange", "mango", "orange", "lemon", "orange"]
+
+
+let add =document.querySelector(".btn-add")
