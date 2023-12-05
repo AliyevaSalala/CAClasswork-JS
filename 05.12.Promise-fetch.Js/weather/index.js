15164a530cf8eeb28704ea67c3d86338ee7239c9
AliyevaSalala <aselale58@gmail.com>
2023-12-05 20:21:26 +0300

    weather

--- 05.12.Promise-fetch.Js/weather/index.js
+++ 05.12.Promise-fetch.Js/weather/index.js
@@ -6,27 +6,29 @@ let search = document.querySelector("#search");
 let searchBtn = document.querySelector(".search-btn");
 let curCity = "baku";
 
-function getWeather() {
+function getWeather(city) {
   fetch(
-    `https://api.weatherapi.com/v1/current.json?key=de4fcc1d06a64857be7101727222212&q=${curCity}&aqi=no`
+    `https://api.weatherapi.com/v1/current.json?key=de4fcc1d06a64857be7101727222212&q=${city}&aqi=no`
   )
     .then((res) => res.json())
-    .then(
-      (data) =>
-        (dataWather.innerHTML = `
+    .then((data) => {
+      if (data.current) {
+        dataWather.innerHTML = `
             <p>${data.location?.name}, ${data.location.country}</p>
             <img src="${data.current.condition.icon}" alt="">
             <p>${data.current.condition.text}</p>
             <p>${data.current.temp_c}</p>
             <p>${data.current.temp_f}</p>
             <p>${data.location.localtime_epoch}</p>
-      `)
-    );
+      `;
+      } else {
+        dataWather.innerHTML = `Belə bir şəhər yoxdur!!!`;
+      }
+    });
 }
-getWeather();
+getWeather("baku");
 
-searchBtn.addEventListener("click", () => {
-  
-  // getWeather(search.value);
-  console.log(search.value);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeather(search.value);
 });
