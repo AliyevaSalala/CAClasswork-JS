7fc4ba469759e06241a4ff15e530df09d1c294ee
AliyevaSalala <aselale58@gmail.com>
2023-12-19 17:28:13 +0300

    continue

--- Floral-Studio/assets/js/main.js
+++ Floral-Studio/assets/js/main.js
@@ -18,3 +18,23 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+
+const menuIcon = document.querySelector("#menu");
+const nav = document.querySelector(".nav");
+const products = document.querySelector(".products");
+
+const BASE_URL = "http://localhost:8080/products";
+
+async function getAllData() {
+  const res = await axios(`${BASE_URL}`);
+  console.log(res.data);
+}
+
+getAllData()
+
+menuIcon.addEventListener("click", function () {
+  nav.classList.toggle("show");
+  this.classList.contains("fa-bars")
+    ? (this.classList = "fa-solid fa-xmark")
+    : (this.classList = "fa-solid fa-bars");
+});
